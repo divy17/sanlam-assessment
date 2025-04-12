7735296253dba87d2ef1581ea3a5786479491bf7
DivyanChetty <Divayn.Chetty@takealot.com>
2025-04-12 12:54:13 +0200

    Fix project structure

---
+++ src/main/java/com/divy/sanlam/SanlamApplication.java
@@ -0,0 +1,13 @@
+package com.divy.sanlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanlamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divy/sanlam/SanlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divy.sanlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanlamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
